9dd981e4daf1d563d08df71bd7c13fdab8b6bf84
MLazizjon <maxmudjonovlazizjon@gmail.com>
2026-03-21 22:09:30 +0500

    o'zgarish haqida qisqa xabar

--- src/pages/logoutStudent/reading/Reading.jsx
+++ src/pages/logoutStudent/reading/Reading.jsx
@@ -1,5 +1,5 @@
 import React, { useState} from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import * as S from './reading.styles';
 // import { FiCheck, FiFlag } from 'react-icons/fi';
 // import ListeningHeader from '../../../components/header/Header'; 
